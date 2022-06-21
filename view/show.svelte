62a4a4596fab0b8f0fb87db54bc27c0b8e83470c
Daryl Manning <daryl@wakatara.com>
2022-06-22 02:42:04 +0800

    [WIP] Tweak to show for comments.

--- view/show.svelte
+++ view/show.svelte
@@ -9,25 +9,7 @@
 
 <Story {story} />
 
-<table border="1" cellpadding="10">
-  <thead>
-    {#each Object.keys(story) as key}
-      <th>{key}</th>
-    {/each}
-  </thead>
-  <tr>
-    {#each Object.keys(story) as key}
-      <td>{story[key]}</td>
-    {/each}
-  </tr>
-</table>
+{#each story.children as comment}
+  <Comment {comment} />
+{/each}
 
-<br />
-
-<a href="">Back</a>
-
-<style>
-  table {
-    border-collapse: collapse;
-  }
-</style>
